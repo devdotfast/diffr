78816a6d3a1293c48b1e817ff86b0150ef83d563
thesiti92 <aiansiti@outlook.com>
2026-09-21 15:59:28 -0400

    Remove difftastic's manual, homepage, man page and citation

--- manual/replace_version_placeholder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DFT_VERSION=$(cargo read-manifest | jq -r .version)
-
-jq --arg version "$DFT_VERSION" '.[1] | walk(if type == "string" then gsub("DFT_VERSION_HERE"; $version) else . end)'
